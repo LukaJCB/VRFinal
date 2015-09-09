0d0b2310ddb7ff3f25c71ba97cba55a9ec9e5d63
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2015-09-09 22:24:12 +0200

    Adds concept of lives and dying

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int startingLives;
+	private int currentLives;
+
+	// Use this for initialization
+	void Start () {
+		currentLives = startingLives;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag("LaserBeam")){
+			ApplyDamage();
+		}
+	
+	}
+
+	void ApplyDamage(){
+
+		BroadcastMessage ("RemainingLives", currentLives - 1);
+		Debug.Log (currentLives);
+
+		if (currentLives > 1) {		
+			currentLives--;
+		} else {
+			LoadMainMenu();
+		}
+	}
+
+	void LoadMainMenu(){
+
+		Application.LoadLevel (0);
+	}
+
+}
